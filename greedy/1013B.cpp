7be3fbd901e6007d4557e24641f482405b6291ff
Nimtaa <nima.tavassoli2@gmail.com>
2018-08-17 23:48:28 +0430

    bs added

--- greedy/1013B.cpp
+++ greedy/1013B.cpp
@@ -3,6 +3,37 @@
 
 #include<iostream>
 using namespace std;
+
+int binarySearch(int arr[], int l, int r, int x)
+{
+   if (r >= l)
+   {
+        int mid = l + (r - l)/2;
+        if (arr[mid] == x)
+            return mid;
+        if (arr[mid] > x)
+            return binarySearch(arr, l, mid-1, x);
+
+        return binarySearch(arr, mid+1, r, x);
+   }
+   return -1;
+}
+
 int main(){
-  
+  int n,x;
+  cin >> n >> x;
+  int array[n];
+  for(int i=0;i<n;i++){
+    cin>>array[i];
+  }
+  for(int i=0;i<n;i++){
+    int temp = x && array[i];
+    int index = binarySearch(array,0,n-1,temp);
+    if(index!=-1){
+      cout<<"";
+    }
+
+  }
+
+
 }
